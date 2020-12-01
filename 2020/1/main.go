7b86c3621a40b5004be33fe62d3fcf99975f5aa9
Enrique Lacal <enrique.lacal.bereslawski@ibm.com>
2020-12-01 12:14:19 +0000

    2020: Puzzle 1 - refactor

--- 2020/1/main.go
+++ 2020/1/main.go
@@ -8,6 +8,43 @@ import (
 	"strconv"
 )
 
+const expectSum = 2020
+
+func part1(numbers []int) int {
+	for _, v := range numbers {
+		if v >= expectSum {
+			continue
+		}
+		for _, j := range numbers {
+			if v+j == expectSum {
+				return v * j
+			}
+		}
+	}
+
+	return 0
+}
+
+func part2(numbers []int) int {
+	for _, v := range numbers {
+		if v >= expectSum {
+			continue
+		}
+		for _, j := range numbers {
+			if v+j >= expectSum {
+				continue
+			}
+			for _, k := range numbers {
+				if v+j+k == expectSum {
+					return v * j * k
+				}
+			}
+		}
+	}
+
+	return 0
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -22,20 +59,6 @@ func main() {
 		numbers = append(numbers, number)
 	}
 
-	// Brute force
-	for _, v := range numbers {
-		for _, j := range numbers {
-			for _, k := range numbers {
-				if v+j+k == 2020 {
-					multi := v * j * k
-					fmt.Printf("Result: %v\n", multi)
-					os.Exit(0)
-				}
-			}
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Printf("Part1: %v\n", part1(numbers))
+	fmt.Printf("Part2: %v\n", part2(numbers))
 }
